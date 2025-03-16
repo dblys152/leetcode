471abd60f690a719aec132bdca44cc53f6267ae0
youngseok <48229567+dblys152@users.noreply.github.com>
2025-03-16 13:37:48 +0900

    Time: 1 ms (29.95%), Space: 42.5 MB (26.98%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int loofSize = m + n;
-        int nums2Length = nums2.length;
+        if (nums1.length != loofSize) {
+            System.out.println("...");
+        }
         int nums2Index = 0;
         for (int i = 0; i < loofSize; i++) {
-            if (nums1[i] == 0 && nums2Index < nums2Length) {
+            if (nums1[i] == 0 && nums2Index < n) {
                 nums1[i] = nums2[nums2Index++];
             }
         }
