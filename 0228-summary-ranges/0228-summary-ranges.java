bd17af0ec6f0a62168f96569492c226671e1611c
youngseok <48229567+dblys152@users.noreply.github.com>
2025-03-23 14:53:23 +0900

    Time: 0 ms (100%), Space: 41.5 MB (90.56%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -5,15 +5,17 @@ class Solution {
         }
 
         List<String> results = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         int rangeStartIndex = 0;
         int loofSize = nums.length;
         for (int i = 0; i < loofSize; i++) {
             boolean isEndRange = (i == loofSize - 1) || (nums[i] + 1 != nums[i+1]);
             if (isEndRange) {
                 results.add(rangeStartIndex == i 
-                    ? String.valueOf(nums[i]) 
-                    : nums[rangeStartIndex] + "->" + nums[i]);
+                    ? sb.append(nums[i]).toString() 
+                    : sb.append(nums[rangeStartIndex]).append("->").append(nums[i]).toString());
                 rangeStartIndex = i + 1;
+                sb.setLength(0);
             }
         }
         
